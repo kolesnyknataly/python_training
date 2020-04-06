81f7df42cf9b3038443a90884fd8c1e99fd52fa3
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-04-06 22:45:33 +0300

    add helper class 'session'

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,9 +12,9 @@ def app(request):
 
     
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contacts(first_name="Annaa", middle_name="Maria", last_name="Ivanova", nickname="marie", title="ree", company="test_company", address="trtrtrt", home="64645",
                             mobile="75675", work="8787", fax="55657", email="testing@gna.yt", email_2="hgjgj2TR.UY", homepage="rtete.com", b_year="1993", address_2="ytyt",
                             notes="hghg"))
-    app.logout()
+    app.session.logout()
 
