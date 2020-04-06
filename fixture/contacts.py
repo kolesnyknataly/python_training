0472aa0619fd8ae3ef2ab0f1e33674289a12484e
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-04-06 23:45:15 +0300

    creating classes for working with groups and contacts are highlighted

---
+++ fixture/contacts.py
@@ -0,0 +1,69 @@
+class ContactsHelpers:
+
+    def __init__(self, app):
+        self.app = app
+
+    def open_add_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create(self, contacts):
+        wd = self.app.wd
+        self.open_add_contact_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contacts.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contacts.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contacts.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contacts.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contacts.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contacts.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contacts.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contacts.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contacts.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contacts.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contacts.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contacts.email_2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contacts.homepage)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contacts.address_2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contacts.notes)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,8 +13,8 @@ def app(request):
     
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contacts(first_name="Annaa", middle_name="Maria", last_name="Ivanova", nickname="marie", title="ree", company="test_company", address="trtrtrt", home="64645",
-                            mobile="75675", work="8787", fax="55657", email="testing@gna.yt", email_2="hgjgj2TR.UY", homepage="rtete.com", b_year="1993", address_2="ytyt",
-                            notes="hghg"))
+    app.contacts.create(Contacts(first_name="Annaa", middle_name="Maria", last_name="Ivanova", nickname="marie", title="ree", company="test_company", address="trtrtrt", home="64645",
+                                 mobile="75675", work="8787", fax="55657", email="testing@gna.yt", email_2="hgjgj2TR.UY", homepage="rtete.com", b_year="1993", address_2="ytyt",
+                                 notes="hghg"))
     app.session.logout()
 
