c52ed6c5774cf2ff601a6522f9f2f92c03acab70
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-04-09 22:28:05 +0300

    add test 'add_empty_contact' for correct running all test

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,9 +2,16 @@
 from model.contacts import Contacts
 import time
 
-    
-def test_add_contact(app):
-    app.contacts.create(Contacts(first_name="Annaa", middle_name="Maria", last_name="Ivanova", nickname="marie", title="ree", company="test_company", address="trtrtrt", home="64645",
-                                 mobile="75675", work="8787", fax="55657", email="testing@gna.yt", email_2="hgjgj2TR.UY", homepage="rtete.com", b_year="1993", address_2="ytyt",
-                                 notes="hghg"))
 
+def test_add_contact(app):
+    app.contacts.create(
+        Contacts(first_name="Annaa", middle_name="Maria", last_name="Ivanova", nickname="marie", title="ree",
+                 company="test_company", address="trtrtrt", home="64645",
+                 mobile="75675", work="8787", fax="55657", email="testing@gna.yt", email_2="hgjgj2TR.UY",
+                 homepage="rtete.com", b_year="1993", address_2="ytyt",
+                 notes="hghg"))
+
+
+def test_add_empty_contact(app):
+    app.contacts.create(
+        Contacts(first_name="", middle_name="", last_name="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email_2="",homepage="", b_year="", address_2="", notes=""))
