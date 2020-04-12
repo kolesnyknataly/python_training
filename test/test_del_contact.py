0e0fd246e37a70100b16f46f5614b0506282e1d2
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-04-12 13:44:16 +0300

    Optimize navigation for 'open_groups_page' method

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -6,3 +6,4 @@ def test_delete_first_contact(app):
     if app.contacts.count() == 0:
         app.contacts.create((Contacts(first_name="test", middle_name="test2", last_name="test3", nickname="test4", title="test5", company="test_company", address="trtrtrt", home="64645", mobile="75675", work="8787", fax="55657", email="testing@gna.yt", email_2="hgjgj2TR.UY", homepage="rtete.com", b_year="1993", address_2="ytyt", notes="hghg")))
     app.contacts.delete_first_contact()
+    time.sleep(1)
