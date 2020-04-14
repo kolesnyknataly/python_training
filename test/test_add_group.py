39a3834566887c157ba21f50fd98078db8b05bf6
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-04-14 22:29:52 +0300

    comparison of group list sizes after adding, deleting, editing group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,14 @@ import time
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="wr", header="dgdf", footer="dfgdg"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
