c391d560fc7ee8363cb93d53c6ca4349a43b68af
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-04-27 22:29:35 +0300

    add test 'test_phones_on_contact_view_page'

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -1,4 +1,5 @@
 from model.contacts import Contacts
+import re
 
 
 class ContactsHelpers:
@@ -217,6 +218,17 @@ class ContactsHelpers:
         phone2 = wd.find_element_by_name("phone2").get_attribute("value")
         return Contacts(first_name=first_name, last_name=last_name, id=id, home=home, work=work, mobile=mobile,
                         phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contacts(home=home, work=work, mobile=mobile, phone2=phone2)
+
 
 
 """    def get_contact_list(self):
