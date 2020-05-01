d3fba211d41d725844c67e28f4a7b7b6a00f3639
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-05-01 20:16:29 +0300

    Pass browser and url through commandline

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,21 @@ from fixture.contacts import ContactsHelpers
 
 class Application:
 
-    def __init__(self):
+    def __init__(self, browser, baseUrl):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(2)
         self.session = SessionHelpers(self)
         self.group = GroupHelper(self)
         self.contacts = ContactsHelpers(self)
+        self.baseUrl = baseUrl
 
     def is_valid(self):
         try:
@@ -22,7 +31,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.baseUrl)
 
     def destroy(self):
         self.wd.quit()
