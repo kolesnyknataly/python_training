1367870adb10d6da230eaa738f0162132335ea7d
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-05-10 21:03:21 +0300

    edit test 'edit contact' (check the data downloaded from the database)

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -26,6 +26,12 @@ class ContactsHelpers:
         wd = self.app.wd
         wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
+    def find_contact_for_edit_by_id(self, id):
+        wd = self.app.wd
+        self.open_contacts_page()
+        wd.find_element_by_xpath("//a[@href='edit.php?id=%s']" % id).click()
+
+
     def create(self, contacts):
         wd = self.app.wd
         self.open_add_contact_page()
@@ -107,8 +113,6 @@ class ContactsHelpers:
         wd.switch_to_alert().accept()
         self.contact_cache = None
 
-
-
     def delete_first_contact(self):
         wd = self.app.wd
         self.delete_contact_by_index(0)
@@ -174,6 +178,72 @@ class ContactsHelpers:
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contacts.notes)
+        # submit contact creation
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+        self.contact_cache = None
+
+    def edit_contact_by_id(self, id, contact):
+        wd = self.app.wd
+        self.open_contacts_page()
+        # self.select_contact_by_index(index)
+        # init contact editing
+        self.find_contact_for_edit_by_id(id)
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email_2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email_3)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address_2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
         # submit contact creation
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,24 +2,24 @@ from model.contacts import Contacts
 from random import randrange
 
 
-def test_edit_some_contact(app):
-    if app.contacts.count() == 0:
+def test_edit_some_contact(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contacts.create((Contacts(first_name="test", middle_name="test2", last_name="test3", nickname="test4",
                                       title="test5", company="test_company", address="trtrtrt", home="64645",
                                       mobile="75675", work="8787", fax="55657", email="testing@gna.yt",
                                       email_2="hgjgj2TR.UY", email_3="testin3g@gna.yt",  homepage="rtete.com",
                                       b_year="1993", address_2="ytyt", phone2="phone2", notes="hghg")))
-    old_contacts = app.contacts.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
-    contact = Contacts(first_name="change_first_name", middle_name="change.middle_name", last_name="change.last_name",
+    contact = old_contacts[index]
+    changed_contact = Contacts(first_name="change_first_name", middle_name="change.middle_name", last_name="change.last_name",
                        nickname="change.nickname", title="change.title", company="test_company",
                        address="changeaddress", home="changehome", mobile="changemobile", work="changework",
                        fax="changefax", email="change.email", email_2="change.email_2", email_3="changetestin3g@gna.yt",
                        homepage="change.homepage", b_year="1993", address_2="change.address_2", phone2="changephone2",
-                       notes="changenotes")
-    contact.id = old_contacts[index].id
-    app.contacts.edit_contact_by_index(index, contact)
+                       notes="changenotes", id=contact.id)
+    app.contacts.edit_contact_by_id(changed_contact.id, changed_contact)
     assert len(old_contacts) == app.contacts.count()
-    new_contacts = app.contacts.get_contact_list()
-    old_contacts[index] = contact
+    new_contacts = db.get_contact_list()
+    old_contacts[index] = changed_contact
     assert sorted(old_contacts, key=Contacts.id_or_max) == sorted(new_contacts, key=Contacts.id_or_max)
