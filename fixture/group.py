6de5733e2ab33e346a209fdfdebec00411dd2eb7
Nataliia.Kolesnyk <kolesnyk.nataly@gmail.com>
2020-05-10 19:30:29 +0300

    edit test 'edit group' (check the data downloaded from the database)

--- fixture/group.py
+++ fixture/group.py
@@ -82,6 +82,27 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def edit_group_by_id(self, id, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # init group editing
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def edit_first_group(self):
         self.edit_group_by_index(0)
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,16 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_edit_group_name(app):
-    if app.group.count() == 0:
+def test_edit_group_name(app, db):
+    if len(db.get_group_list()) == 0:
         app.group.create((Group(name="test", header="test2", footer="test3")))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="change_name", header="change_header", footer="change_footer")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(index, group)
+    old_groups = db.get_group_list()
+    index = random.randrange(len(old_groups))
+    group = old_groups[index]
+    changed_group = Group(name="change_name", header="change_header", footer="change_footer", id=group.id)
+    app.group.edit_group_by_id(changed_group.id, changed_group)
+    new_groups = db.get_group_list()
+    old_groups[index] = changed_group
     assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
